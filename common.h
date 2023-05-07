89954433caec3fa80610ce4bfbbbdc63b012ce29
Antoni Pokusiński <apokusinski@o2.pl>
2023-05-07 14:06:16 +0200

    Move receive_t from icmp.h to common.h

--- common.h
+++ common.h
@@ -10,4 +10,11 @@
         exit(1); \
     } while(0)
 
+typedef struct receive {
+    int rec_icmp_type;
+    struct in_addr rec_addr;
+    struct timeval rec_send_time;
+    struct timeval rec_rec_time;
+} receive_t;
+
 #endif /* COMMON_H */
--- icmp.h
+++ icmp.h
@@ -4,14 +4,7 @@
 #include <arpa/inet.h>
 #include <sys/time.h>
 
-
-typedef struct receive {
-    int rec_icmp_type;
-    struct in_addr rec_addr;
-    struct timeval rec_send_time;
-    struct timeval rec_rec_time;
-} receive_t;
-
+#include "common.h"
 
 void send_icmp_echo(int sockfd, struct sockaddr_in* address, int ttl, int seq);
 int receive_icmp(int sockfd, int min_seq, int max_seq, struct timeval* wait_time, receive_t* response);
